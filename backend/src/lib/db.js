83e0d9783cceab0f5ca3f261d6ad5abe9eb36e31
Abhi9125 <abhi.singh3132@gmail.com>
2025-11-13 23:14:57 +0530

    Setup(db, creak, inngest, stream)

---
+++ backend/src/lib/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(ENV.DB_URL);
+
+    console.log("Connected to the MongoDB:", conn.connection.host);
+  } catch (err) {
+    console.error("❌ Connection Error");
+
+    process.exit(1); // 0 means success and 1 means exit
+  }
+};
