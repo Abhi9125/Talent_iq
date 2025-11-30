e227ef1d6a3f905a53da46ce1165afd1499a39dc
Abhi9125 <abhi.singh3132@gmail.com>
2025-12-01 02:42:49 +0530

    sync cleark with inngest

---
+++ backend/src/models/User.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    profilePic: {
+      type: String,
+      require: true,
+    },
+    clearId: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
